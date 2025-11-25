bf383dc69df654fb07a8b20f8aacffa53362ae01
DREXATROLL <andrewulyanov26@gmail.com>
2025-11-26 02:01:28 +0300

    Add models to admin

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Instrument, Rental
 
-# Register your models here.
+# Настройка отображения Инструментов
+@admin.register(Instrument)
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'status') # Какие колонки показывать
+    list_filter = ('status', 'category') # Фильтры справа
+    search_fields = ('name',) # Строка поиска
+
+# Настройка отображения Аренд
+@admin.register(Rental)
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ('instrument', 'user', 'created_at')
+    readonly_fields = ('created_at',) # Запрет на редактирование даты создания
