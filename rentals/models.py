c0ebe3cbbba72e33f6e4336a141a5e03ac83a64c
DREXATROLL <andrewulyanov26@gmail.com>
2025-12-10 09:57:09 +0300

    test 1

--- rentals/models.py
+++ rentals/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# --- СПРАВОЧНИКИ 
+# СПРАВОЧНИКИ 
 
 class Category(models.Model):
     name = models.CharField("Название категории", max_length=100)
