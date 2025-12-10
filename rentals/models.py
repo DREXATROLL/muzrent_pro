57138a09a6656be83c8c9f33d4c5420ee0830a1e
DREXATROLL <andrewulyanov26@gmail.com>
2025-12-10 09:58:37 +0300

    Update DB schema to 11 tables

--- rentals/models.py
+++ rentals/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# СПРАВОЧНИКИ 
+# cСПРАВОЧНИКИ 
 
 class Category(models.Model):
     name = models.CharField("Название категории", max_length=100)
